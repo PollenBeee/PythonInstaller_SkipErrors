e710d4647fa355a4b57fe6d746823ed9319af1b2
Pradeep <11526434+PollenBeee@users.noreply.github.com>
2024-12-26 10:08:03 +0530

    Create installer.py

---
+++ installer.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from pip import main
+
+error_log = open('error_log.txt', 'w')
+
+def install(package):
+    try:
+        main(['install'] + [str(package)])
+    except Exception as e:
+        error_log.write(str(e))
+
+if __name__ == '__main__':
+    f = open('requirements.txt', 'r')
+    for line in f:
+        install(line)
+    f.close()
+    error_log.close()
